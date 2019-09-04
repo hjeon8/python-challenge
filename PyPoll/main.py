d3a13e47ca5c2e89787b97a0fa6f3cf19a4ce017
hjeon8 <53793780+hjeon8@users.noreply.github.com>
2019-09-04 15:40:07 -0400

    Update main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,8 +1,48 @@
 import os
 import csv
 
-budget_data = os.path.join('..', 'Resources', 'election_data.csv')
-a = budget_data["Voter ID"].count()
-b = training_data["Candidate"].value_counts()
-percent = b/a*100
-winner = index(max(b))
+poll_data = os.path.join("...", "Resources", "election_data.csv")
+
+
+total_votes = 0
+candidates_unique = []
+candidate_vote_count = []
+
+with open(poll_data, newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    csv_header = next(csvreader)
+    
+    for row in csvreader:
+        total_votes += 1
+        candidate_in = (row[2])
+        if candidate_in in candidates_unique:
+            candidate_index = candidates_unique.index(candidate_in)
+            candidate_vote_count[candidate_index] = candidate_vote_count[candidate_index] + 1
+        else:
+            candidates_unique.append(candidate_in)
+            candidate_vote_count.append(1)
+
+            
+pct = []
+max_votes = candidate_vote_count[0]
+max_index = 0
+
+for x in range(len(candidates_unique)):
+    vote_pct = round(candidate_vote_count[x]/total_votes*100, 2)
+    pct.append(vote_pct)
+    
+    if candidate_vote_count[x] > max_votes:
+        max_votes = candidate_vote_count[x]
+        max_index = x
+
+election_winner = candidates_unique[max_index] 
+
+print("Election Results")
+print("-------------------------------")
+print(f'Total Votes: {total_votes}')
+print("-------------------------------")
+for x in range(len(candidates_unique)):
+    print(f'{candidates_unique[x]} : {pct[x]}% ({candidate_vote_count[x]})')
+print("-------------------------------")
+print(f'Winner: {election_winner.upper()}')
+print("-------------------------------")
