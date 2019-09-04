267228ba8151137074f70e8ed17d0e9df2d06356
hjeon8 <53793780+hjeon8@users.noreply.github.com>
2019-09-04 12:31:29 -0400

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,3 +10,37 @@ budget_data["Profit/Losses"].mean()
 budgetmax = budget_data["Profit/Losses"].max()
 budget_data["Profit/Losses"].min()
 budget_data["Date"].index(max(budgetmax))
+
+
+    P = []
+    months = []
+
+    #read through each row of data after header
+    for rows in csvreader:
+        P.append(int(rows[1]))
+        months.append(rows[0])
+
+    # find revenue change
+    revenue_change = []
+
+    for x in range(1, len(P)):
+        revenue_change.append((int(P[x]) - int(P[x-1])))
+    
+    # calculate average revenue change
+    revenue_average = sum(revenue_change) / len(revenue_change)
+    
+    # calculate total length of months
+    total_months = len(months)
+
+    # greatest increase in revenue
+    greatest_increase = max(revenue_change)
+    # greatest decrease in revenue
+    greatest_decrease = min(revenue_change)
+    
+    print("Financial Analysis")
+    print("-------------------------")
+    print("Total Months: " + str(total_months))
+    print("Total Revenue: " + "$" + str(total_revenue))
+    print("Average Change: " + "$" + str(round(sum(revenue_changes) / len(revenue_changes),2)))
+    print("Greatest Increase: " + str(greatest_increase[0]) + " ($" +  str(greatest_increase[1]) + ")") 
+    print("Greatest Decrease: " + str(greatest_decrease[0]) + " ($" +  str(greatest_decrease[1]) + ")")
