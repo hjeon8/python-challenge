48d7e1b39288652cf5e2ca18b2d26119ebf5661b
hjeon8 <53793780+hjeon8@users.noreply.github.com>
2019-09-04 14:25:10 -0400

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,46 +1,36 @@
 import os
 import csv
 
-budget_data = os.path.join('..', 'Resources', 'budget_data.csv')
+budget_data = os.path.join("C:/Users/Layla Jeon/Desktop/GT-PTP-DATA-PT-08-2019-U-C/02-Homework/03-Python/Instructions/PyBank/Resources/budget_data.csv")
 
-
-budget_data["Date"].count()
-budget_data["Profit/Losses"].sum()
-budget_data["Profit/Losses"].mean()
-budgetmax = budget_data["Profit/Losses"].max()
-budget_data["Profit/Losses"].min()
-budget_data["Date"].index(max(budgetmax))
-
+with open(budget_data, newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    csv_header = next(csvfile)
+   
 
     P = []
     months = []
 
-    #read through each row of data after header
     for rows in csvreader:
         P.append(int(rows[1]))
         months.append(rows[0])
 
-    # find revenue change
     revenue_change = []
 
     for x in range(1, len(P)):
         revenue_change.append((int(P[x]) - int(P[x-1])))
     
-    # calculate average revenue change
     revenue_average = sum(revenue_change) / len(revenue_change)
     
-    # calculate total length of months
     total_months = len(months)
 
-    # greatest increase in revenue
     greatest_increase = max(revenue_change)
-    # greatest decrease in revenue
     greatest_decrease = min(revenue_change)
     
     print("Financial Analysis")
     print("-------------------------")
     print("Total Months: " + str(total_months))
-    print("Total Revenue: " + "$" + str(total_revenue))
-    print("Average Change: " + "$" + str(round(sum(revenue_changes) / len(revenue_changes),2)))
-    print("Greatest Increase: " + str(greatest_increase[0]) + " ($" +  str(greatest_increase[1]) + ")") 
-    print("Greatest Decrease: " + str(greatest_decrease[0]) + " ($" +  str(greatest_decrease[1]) + ")")
+    print("Total Revenue: " + "$" + str((sum(P))))
+    print("Average Change: " + "$" + str(revenue_average))
+    print("Greatest Increase in Profits: " + str(months[revenue_change.index(max(revenue_change))+1]) + " " + "$" + str(greatest_increase))
+    print("Greatest Decrease in Profits: " + str(months[revenue_change.index(min(revenue_change))+1]) + " " + "$" + str(greatest_decrease))
